e1dc58c3cfd1d4aec0c5b598c619696a0ff7e413
Sivatheking <105096041+Sivatheking@users.noreply.github.com>
2022-07-09 14:32:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from pyrogram import Client, filters
 
+API_ID="17795696"
+API_HASH="afcbcd249c7d8728d9b213b10d39a649"
+BOT_TOKEN="5324288989:AAEZNpMAf6DOHveFMOjm-Mxmeu_9TgVBsSg"
+
+Siva = Client(
+   name="PyrogramBot",
+   api_id=API_ID,
+   api_hash=API_HASH,
+   bot_token=BOT_TOKEN
+)
+print("Bot strarted")
+
+Siva.run()
