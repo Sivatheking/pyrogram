909fe87f235ac137251a385f879529f7d5714f6c
Sivatheking <105096041+Sivatheking@users.noreply.github.com>
2022-07-10 08:47:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 
 API_ID="17795696"
 API_HASH="afcbcd249c7d8728d9b213b10d39a649"
