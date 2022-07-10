ca02e897b705828c9fe9b7b35a7f2517ee9068b6
Sivatheking <105096041+Sivatheking@users.noreply.github.com>
2022-07-10 08:41:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
+import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 
 API_ID="17795696"
 API_HASH="afcbcd249c7d8728d9b213b10d39a649"
