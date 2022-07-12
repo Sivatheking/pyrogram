686f9d3a092ebcdad986581c7930cd6dafed21ad
Sivatheking <105096041+Sivatheking@users.noreply.github.com>
2022-07-12 17:01:27 +0530

    Update main.py

--- Pyrogram/main.py
+++ Pyrogram/main.py
@@ -14,7 +14,7 @@ Siva = Client(
 @Siva.on_message(filters.command("start"))
 async def start_cmd(client, message):
    await message.reply_text(
-       text=USER_MENTION)
+       text=PM_MENTION)
 
 
 
