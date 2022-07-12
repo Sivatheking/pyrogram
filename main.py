734cd4dbc2ffad1fd3c03e397ac65a5136e5395c
Sivatheking <105096041+Sivatheking@users.noreply.github.com>
2022-07-12 13:18:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ Siva = Client(
    api_hash=API_HASH,
    bot_token=BOT_TOKEN
 )
-@Siva.on_message(filters.group & filters.private("start"))
+@Siva.on_message(filters.group & filters.command("start"))
 async def start_cmd(client, message):
    await message.reply_text(
        text=f"""Hello! Iam pyrogram bot {message.from_user.mention}""",
