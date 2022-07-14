0b73a14ea4c3927f821ac1085a2af3a815258c0c
Sivatheking <105096041+Sivatheking@users.noreply.github.com>
2022-07-14 13:12:04 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -37,7 +37,8 @@ async def help_cmd(client, message):
     await message.reply_photo(
         photo="https://telegra.ph/file/e3975e328fad64bd75b18.jpg",
         caption="<b> only telegram</b>",
-        reply_markup=InlineKeyboardMarkup(Button)
+        reply_markup=InlineKeyboardMarkup(Button),
+        alias="help"
     )
 
 
