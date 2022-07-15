eda30835d18bafde403c386608d3f9b377f28834
Sivatheking <105096041+Sivatheking@users.noreply.github.com>
2022-07-15 20:31:49 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,4 +1,4 @@
-from pyrogram import Client, filters
+from pyrogram import Client, filters, Messages 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant 
 
