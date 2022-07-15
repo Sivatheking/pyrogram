81ccd1de1767d09c3fe34b510a5ed127c3b2a5ca
Sivatheking <105096041+Sivatheking@users.noreply.github.com>
2022-07-15 22:30:06 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -39,7 +39,7 @@ Button = [[
 async def start_cmd(client, message):
     if Forse_channel:
         try:
-            user = await client.get_chat_member(Forse_channel, message.from_use.id)     
+            user = await client.get_chat_member("Forse_channel, message.from_use.id")     
             if user.status == "kicked out":
                 await message.reply_text("You are banned")
                 return
