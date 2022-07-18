9ec49b3b2f411af93e9dec8f63b6af6d84610ede
Sivatheking <105096041+Sivatheking@users.noreply.github.com>
2022-07-18 20:49:25 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -73,7 +73,8 @@ async def start_cmd(client, message):
             ]]
         )
     )
-
+@Siva.on_message(filters.regex("RRR movie"))
+async start_keyboard(client, message):
     await message.reply_photo(
         photo="https://telegra.ph/file/e3975e328fad64bd75b18.jpg",
         caption="Move : RRR\nLanguage : multi audios\nType : inline \nUpload by : @Sivatheboss",
