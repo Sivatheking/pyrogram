d06d6b99350f9be96d34c340c99138cb0c1b8c6f
Sivatheking <105096041+Sivatheking@users.noreply.github.com>
2022-08-03 07:25:58 +0530

    Update Sample_config.py

--- bot/Sample_config.py
+++ bot/Sample_config.py
@@ -2,8 +2,8 @@ import os
 
 class Config(object):
      
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", 12345))
-API_HASH = os.environ.get("API_HASH", "")
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "mo_tech_YT")
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "mt_id_Bot")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5533113564:AAGszACC2lzfkjiWbppFL-qMZ86jtp7ryVY")
+    API_ID = int(os.environ.get("API_ID", 17795696))
+    API_HASH = os.environ.get("API_HASH", "afcbcd249c7d8728d9b213b10d39a649")
+    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Pytogram_support")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Emlifhbot")
