4dd2f1714d951ab88bd30267e0f385234b75624e
Sivatheking <105096041+Sivatheking@users.noreply.github.com>
2022-08-21 19:49:04 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -8,7 +8,7 @@ import script
 
 Siva = Client(
    name="PyrogramBot",
-    token = os.environ["TOKEN"],
+    bot_token = os.environ["BOT_TOKEN"],
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"],
 )
