56441814fe619ef26cd10d898e511c0d9a22393b
Sivatheking <105096041+Sivatheking@users.noreply.github.com>
2022-08-22 13:02:53 +0530

    Update Sample_config.py

--- bot/Sample_config.py
+++ bot/Sample_config.py
@@ -1,9 +1,7 @@
-import os
+import os, re
 
-class Config(object):
-     
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5533113564:AAGszACC2lzfkjiWbppFL-qMZ86jtp7ryVY")
-    API_ID = int(os.environ.get("API_ID", 17795696))
-    API_HASH = os.environ.get("API_HASH", "afcbcd249c7d8728d9b213b10d39a649")
-    SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Pytogram_support")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Emlifhbot")
+# Account
+API_HASH = os.environ.get("API_HASH", "5b1d0992294a67cb54512a4fafeb0c88")
+API_ID = os.environ.get("API_ID", "6170803")
+# About Bot
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "1905228806:AAH2iahJcg5J6bqpcW11jB9KajwHbIjaslGTY")
